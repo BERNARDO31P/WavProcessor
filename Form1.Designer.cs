c2b1e808252bb2cbcf3fa7ed973920383ba0ca36
Bernardo de Oliveira <bernardo@bernardo.fm>
2024-06-27 20:29:49 +0200

    Initial release

---
+++ Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace WavProcessor
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label lblStatus;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            //
+            // btnSelectFolder
+            //
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(160, 40);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            //
+            // lblStatus
+            //
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 65);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(82, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status: Idle";
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 100);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = "WAV Processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WavProcessor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowser.SelectedPath;
+                    lblStatus.Text = "Status: Processing...";
+                    await Task.Run(() => ProcessWavFiles(selectedPath));
+                    lblStatus.Text = "Status: Completed!";
+                }
+            }
+        }
+
+        private void ProcessWavFiles(string folderPath)
+        {
+            var wavFiles = Directory.GetFiles(folderPath, "*.wav", SearchOption.AllDirectories).OrderBy(f => Guid.NewGuid()).ToArray();
+
+            foreach (var file in wavFiles)
+            {
+                lblStatus.Invoke((MethodInvoker)(() => lblStatus.Text = "Status: Processing: " + file));
+                ProcessWavFile(file);
+            }
+        }
+
+        private void ProcessWavFile(string filePath)
+        {
+            byte[] data = File.ReadAllBytes(filePath);
+            int fmtChunkOffset = IndexOf(data, new byte[] { (byte)'f', (byte)'m', (byte)'t', (byte)' ' });
+            if (fmtChunkOffset != -1)
+            {
+                int audioFormatOffset = fmtChunkOffset + 8;
+                ushort audioFormat = BitConverter.ToUInt16(data, audioFormatOffset);
+                if (audioFormat == 0xfffe)
+                {
+                    byte[] newAudioFormat = BitConverter.GetBytes((ushort)1);
+                    Array.Copy(newAudioFormat, 0, data, audioFormatOffset, newAudioFormat.Length);
+                }
+            }
+
+            File.WriteAllBytes(filePath, data);
+        }
+
+        private int IndexOf(byte[] array, byte[] pattern)
+        {
+            int maxFirstCharSlot = array.Length - pattern.Length + 1;
+            for (int i = 0; i < maxFirstCharSlot; i++)
+            {
+                if (array[i] != pattern[0]) // Compare only first byte
+                    continue;
+
+                // Found a match on first byte, now try to match rest of the pattern
+                for (int j = pattern.Length - 1; j >= 1; j--)
+                {
+                    if (array[i + j] != pattern[j])
+                        break;
+                    if (j == 1)
+                        return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
